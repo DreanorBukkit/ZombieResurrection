421f27511eceb02e3157c5c40ce2aa7153c04646
Dreanor <mikeundead@googlemail.com>
2013-03-20 22:43:38 +0100

    Revert "implemented support for multiworld"
    This reverts commit 485aa1db0037a2ab94215ebc272e0e35a01a4fd5.

--- src/com/gmail/mikeundead/ZombieResurrection.java
+++ src/com/gmail/mikeundead/ZombieResurrection.java
@@ -1,42 +1,22 @@
 package com.gmail.mikeundead;
 
-import multiworld.MultiWorldPlugin;
-import multiworld.api.MultiWorldAPI;
-
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 import com.gmail.mikeundead.Handlers.ConfigHandler;
 import com.gmail.mikeundead.Listeners.EntityDeathListener;
 
-public class ZombieResurrection extends JavaPlugin implements Listener
+public class ZombieResurrection extends JavaPlugin
 {
-	private EntityDeathListener entityDeathListener;
-
-    @Override
-	public void onEnable()
+    public void onEnable()
     {
-		ConfigHandler configHandler = new ConfigHandler(this);
-        this.entityDeathListener = new EntityDeathListener(configHandler);
+        ConfigHandler configHandler = new ConfigHandler(this);
+        EntityDeathListener entityDeathListener = new EntityDeathListener(configHandler);
+        this.getServer().getPluginManager().registerEvents(entityDeathListener, this);
 
-        this.getServer().getPluginManager().registerEvents(this.entityDeathListener, this);
-        this.getServer().getPluginManager().registerEvents(this, this);
         this.getLogger().info("ZombieResurrection has been enabled.");
 	}
 
-    @EventHandler
-    public void onWorldLoad(WorldLoadEvent event)
-    {
-    	MultiWorldAPI multiWorld = ((MultiWorldPlugin)this.getServer().getPluginManager().getPlugin("MultiWorld")).getApi();
-    	if(multiWorld != null)
-    	{
-			this.entityDeathListener.SetMultiWorld(multiWorld);
-    	}
-    }
-
-	@Override
 	public void onDisable()
     {
         this.getLogger().info("ZombieResurrection has been disabled.");
